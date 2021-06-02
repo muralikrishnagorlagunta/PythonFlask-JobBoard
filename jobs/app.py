3b2e896bbc9558cdb07228530016a8dd87d0decc
Murali <muralikrishna.gorlagunta@gmail.com>
2021-06-02 06:06:13 +0000

    3rd check

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
 
 app=Flask(__name__)
 
+PATH='db/jobs.sqlite'
 
 @app.route('/')
 @app.route('/jobs')
