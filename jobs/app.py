fa85167b1122204c108178315c7c9f97365be463
Murali <muralikrishna.gorlagunta@gmail.com>
2021-06-02 06:10:06 +0000

    3rd check

--- jobs/app.py
+++ jobs/app.py
@@ -10,3 +10,11 @@ PATH='db/jobs.sqlite'
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
+
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
