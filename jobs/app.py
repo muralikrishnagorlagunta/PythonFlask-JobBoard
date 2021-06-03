f8b1ba1fb3450132e5a84197bb900bbee94ee1f0
Murali <muralikrishna.gorlagunta@gmail.com>
2021-06-03 21:39:59 +0000

    4th check-2

--- jobs/app.py
+++ jobs/app.py
@@ -11,8 +11,9 @@ PATH = 'db/jobs.sqlite'
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
-                'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, '
+                       'job.salary, employer.id as employer_id, employer.name as employer_name '
+                       'FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
 
 
